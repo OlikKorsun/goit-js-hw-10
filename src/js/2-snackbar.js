ed065433ad7569306f473d2348041a25c7b19a67
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-02-19 03:22:41 +0200

    snackbar - done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,52 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-console.log("snackbar");
+const ourForm = document.querySelector(".form");
+
+ourForm.addEventListener("submit", createPromise);
+
+function createPromise(event) {
+    event.preventDefault();
+    const delay = Number.parseInt(ourForm.delay.value);
+
+    const newPromise = new Promise((resolve, reject) => {
+    
+        setTimeout( () => {
+            if (ourForm.elements.state.value === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+                }
+        }, delay);
+    })
+
+    newPromise
+        .then(() => {
+            iziToast.success({
+                title: 'OK',
+                message: `Fulfilled promise in ${delay} ms`,
+                messageColor: 'white',
+                messageSize: '16',
+                backgroundColor: 'green',
+                theme: 'dark',
+                iconUrl: '../img/ok.svg',
+                position: 'topRight',
+            });
+        })
+        .catch(() => {
+            iziToast.error({
+                title: 'Error',
+                message: `Rejected promise in ${delay} ms`,
+                messageColor: 'white',
+                messageSize: '16',
+                backgroundColor: 'red',
+                theme: 'dark',
+                iconUrl: '../img/error.svg',
+                position: 'topRight',
+            });
+        })
+        .finally( () => {
+            ourForm.reset();
+        });
+}
+
